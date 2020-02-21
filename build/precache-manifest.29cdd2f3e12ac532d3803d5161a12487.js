ff7beb3501a8f7629175d3e9099a019a65131b86
Jusharra <Jusharra@users.noreply.github.com>
2020-02-20 23:24:24 -0800

    my-social-network
    my-social-network app

---
+++ build/precache-manifest.29cdd2f3e12ac532d3803d5161a12487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7f161ee01e3ed3d15c447776aeb9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "213979ebccefc68a3711",
+    "url": "/static/css/2.eae286e1.chunk.css"
+  },
+  {
+    "revision": "b334ba9d3ea4a99b8baa",
+    "url": "/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "213979ebccefc68a3711",
+    "url": "/static/js/2.b5587f73.chunk.js"
+  },
+  {
+    "revision": "bd48e7aad8ffbde0152bdbc9a20111c5",
+    "url": "/static/js/2.b5587f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b334ba9d3ea4a99b8baa",
+    "url": "/static/js/main.29b80ac1.chunk.js"
+  },
+  {
+    "revision": "2754f4ad6517180663e3",
+    "url": "/static/js/runtime-main.a1ee54e0.js"
+  }
+]);
